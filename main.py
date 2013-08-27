1bf466968f01d60e6c54a54fe573fb510f152263
JC <john.carruthers0@gmail.com>
2013-08-27 11:35:28 -0400

    Housekeeping
    Who doesn't like some housekeeping =D

--- main.py
+++ main.py
@@ -3,9 +3,97 @@ import socket
 import binascii
 import sys
 import re
- 
-pcapFileName = 'test2.pcap'
 
+####################
+## FILE FUNCTIONS ##
+####################
+def readFile(filename):
+  try:
+    f = open(filename, 'rb')
+    return f
+  except:
+    print "ERROR: Could Not Open:", filename
+    return False
+  
+def closeFile(handle):
+  try:
+    return handle.close()
+  except:
+    return False
+
+####################
+## PCAP FUNCTIONS ##
+####################
+def openPCAP(fileHandle):
+  #import dpkt
+  return dpkt.pcap.Reader(fileHandle)
+ 
+def iteratePCAP(pcap,callbacks=[]):
+  #import dpkt
+  data = set([])
+  for ts, buff in pcap:
+    try:
+      eth = dpkt.ethernet.Ethernet(buff)
+      
+      for callback in callbacks:
+        for item in callback(eth):
+          data.add(item)
+    except: 
+      pass
+
+  return data
+
+def parseData(data,filters=[]):
+  parsedData = set([])
+  for item in data:
+    for f in filters:
+      if f(item):
+        parsedData.add(item)
+  return parsedData
+
+###############
+## CALLBACKS ##
+###############
+def ethFrameToIPs(eth):  
+  #import socket
+  IPs = []  
+  try:
+    ip = eth.data
+    try:
+      IPs.append(socket.inet_ntoa(ip.src))
+    except: pass
+    try:
+      IPs.append(socket.inet_ntoa(ip.dst))
+    except: pass
+  except: pass
+  return IPs
+
+def ethFrameToDomains(eth):
+  #import dpkt
+  data = []
+  try:
+    ip = eth.data
+    if eth.type == 2048:
+      if ip.p == 17:
+        udp = ip.data
+        if udp.sport == 53 or udp.dport == 53: 
+          dns = dpkt.dns.DNS(udp.data)
+          if dns.qr == dpkt.dns.DNS_R: 
+            if dns.opcode == dpkt.dns.DNS_QUERY: 
+              if dns.rcode == dpkt.dns.DNS_RCODE_NOERR: 
+                if len(dns.an) > 0: 
+                  for answer in dns.an:
+                    data.append(answer.name)
+                    if answer.type == 5: data.append(answer.cname)
+                    elif answer.type == 1: data.append(socket.inet_ntoa(answer.rdata))
+                    elif answer.type == 12: data.append(answer.ptrname)
+  except: 
+    pass
+  return data
+
+#############
+## FILTERS ##
+#############
 def f_NotRFC1918(ip):
   if f_IPv4(ip):
     try:
@@ -51,93 +139,21 @@ def f_ValidatedDomains(name):
   except:
       return False
 
-def readFile(filename):
-  try:
-    f = open(filename, 'rb')
-    return f
-  except:
-    return False
-  
-def openPCAP(fileHandle):
-  #import dpkt
-  return dpkt.pcap.Reader(fileHandle)
-
-def closeFile(handle):
-  try:
-    return handle.close()
-  except:
-    return False
-
-def ethFrameToIPs(eth):  
-  #import socket
-  IPs = []  
-  try:
-    ip = eth.data
-    try:
-      IPs.append(socket.inet_ntoa(ip.src))
-    except: pass
-    try:
-      IPs.append(socket.inet_ntoa(ip.dst))
-    except: pass
-  except: pass
-  return IPs
-
-def ethFrameToDomains(eth):
-  #import dpkt
-  data = []
-  try:
-    ip = eth.data
-    if eth.type == 2048:
-      if ip.p == 17:
-        udp = ip.data
-        if udp.sport == 53 or udp.dport == 53: 
-          dns = dpkt.dns.DNS(udp.data)
-          if dns.qr == dpkt.dns.DNS_R: 
-            if dns.opcode == dpkt.dns.DNS_QUERY: 
-              if dns.rcode == dpkt.dns.DNS_RCODE_NOERR: 
-                if len(dns.an) > 0: 
-                  for answer in dns.an:
-                    data.append(answer.name)
-                    if answer.type == 5: data.append(answer.cname)
-                    elif answer.type == 1: data.append(socket.inet_ntoa(answer.rdata))
-                    elif answer.type == 12: data.append(answer.ptrname)
-  except: 
-    pass
-  return data
- 
-def iteratePCAP(pcap,callbacks=[]):
-  #import dpkt
-  data = set([])
-  for ts, buff in pcap:
-    try:
-      eth = dpkt.ethernet.Ethernet(buff)
-      
-      for callback in callbacks:
-        for item in callback(eth):
-          data.add(item)
-    except: 
-      pass
-
-  return data
-
-def parseData(data,filters=[]):
-  parsedData = set([])
-  for item in data:
-    for f in filters:
-      if f(item):
-        parsedData.add(item)
-  return parsedData
 
 
-
-
+pcapFileName = 'demo.pcap'
 h = readFile(pcapFileName)
 pcap = openPCAP(h)
 
 x = iteratePCAP(pcap,[ethFrameToIPs,ethFrameToDomains])
 
-print "No-RFC1918:", len(parseData(x,filters=[f_NotRFC1918]))
 print "All IPv4:", len(parseData(x,filters=[f_IPv4]))
+print "No-RFC1918:", len(parseData(x,filters=[f_NotRFC1918]))
 print "All Domains:", len(parseData(x,filters=[f_Domains]))
 print "Validiated Domains:", len(parseData(x,filters=[f_ValidatedDomains]))
+
+print "All IPv4:\n", (parseData(x,filters=[f_IPv4]))
+print "No-RFC1918:\n", (parseData(x,filters=[f_NotRFC1918]))
+print "All Domains:\n", (parseData(x,filters=[f_Domains]))
+print "Validiated Domains:\n", (parseData(x,filters=[f_ValidatedDomains]))
 closeFile(h)
